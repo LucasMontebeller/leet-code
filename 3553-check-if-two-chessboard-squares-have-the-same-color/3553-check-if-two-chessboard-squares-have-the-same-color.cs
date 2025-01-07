69092cf940fd25a9de8970219212a94443ef7344
Lucas Montebeller <81781963+LucasMontebeller@users.noreply.github.com>
2025-01-07 17:27:02 -0300

    Time: 56 ms (60.87%), Space: 42.6 MB (10.87%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cs
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cs
@@ -1,17 +1,10 @@
 public class Solution {
-    private readonly static Dictionary<char, int> _alphabet = new()
-    {
-        { 'a', 0 }, { 'b', 1 }, { 'c', 2 }, { 'd', 3 },
-        { 'e', 4 }, { 'f', 5 }, { 'g', 6 }, { 'h', 7 },
-    };
+    private readonly static char[] _alphabet = "abcdefgh".ToCharArray();
 
-
-    public bool CheckTwoChessboards(string coordinate1, string coordinate2)
-    {
-        _alphabet.TryGetValue(coordinate1[0], out var letterIndex1);
-        _alphabet.TryGetValue(coordinate2[0], out var letterIndex2);
+    public bool CheckTwoChessboards(string coordinate1, string coordinate2) {
+        var letterIndex1 = Array.IndexOf(_alphabet, coordinate1[0]);
+        var letterIndex2 = Array.IndexOf(_alphabet, coordinate2[0]);
 
         return (letterIndex1 + coordinate1[1] - '0') % 2 == (letterIndex2 + coordinate2[1] - '0') % 2;
     }
-        
 }
