42f64da9b4f76c8187e9c8f64b35c70650d49006
Lucas Montebeller <81781963+LucasMontebeller@users.noreply.github.com>
2025-01-07 17:23:01 -0300

    Time: 61 ms (39.13%), Space: 41.8 MB (43.48%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cs
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cs
@@ -1,10 +1,5 @@
 public class Solution {
-    private readonly static char[] _alphabet = "abcdefgh".ToCharArray();
-
-    public bool CheckTwoChessboards(string coordinate1, string coordinate2) {
-        var letterIndex1 = Array.IndexOf(_alphabet, coordinate1[0]);
-        var letterIndex2 = Array.IndexOf(_alphabet, coordinate2[0]);
-
-        return (letterIndex1 + coordinate1[1] - '0') % 2 == (letterIndex2 + coordinate2[1] - '0') % 2;
-    }
+    public bool CheckTwoChessboards(string coordinate1, string coordinate2)
+        => (coordinate1[0] - 'a' + coordinate1[1] - '0') % 2 == (coordinate2[0] - 'a' + coordinate2[1] - '0') % 2;
+        
 }
