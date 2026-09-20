f1c37f8d6a3280ca67cbf9e2546dbc53fe0a69cc
Lucas Montebeller <81781963+LucasMontebeller@users.noreply.github.com>
2026-09-20 12:03:37 -0300

    Time: 87 ms (76.44%), Space: 49.5 MB (13.79%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        HashSet<ListNode> appearances = new HashSet<ListNode>();
+        while (head != null)
+        {
+            if (appearances.Contains(head))
+            {
+                return head;
+            }
+
+            appearances.Add(head);
+            head = head.next;
+        }
+
+        return null;
+    }
+}
